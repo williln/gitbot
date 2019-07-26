86bf0738b8bd938cdb82ffb37addaa38cb6cbd51
Lacey Williams Henschel <laceynwilliams@gmail.com>
2019-07-26 11:24:00 -0500

    Add webhook, gitignore

--- gitbot/__main__.py
+++ gitbot/__main__.py
@@ -1,14 +1,42 @@
+import aiohttp
 import os
 
 from aiohttp import web
+from gidgethub import routing, sansio
+from gidgethub import aiohttp as gh_aiohttp
 
 
+router = routing.Router()
 routes = web.RouteTableDef()
 
 
-@routes.get("/")
+@router.register("issues", action="opened")
+async def issue_opened_event(event, gh, *args, **kwargs):
+    """ Whenever an issue is opened, greet the author and say thanks """
+    url = event.data["issue"]["comments_url"]
+    author = event.data["issue"]["user"]["login"]
+    message = f"Thanks for the report @{author}! (I am a bot.)"
+    await gh.post(url, data={"body": message})
+
+
+@routes.post("/")
 async def main(request):
-    return web.Response(status=200, text="Hello world!")
+    # Read the GitHub webhook payload
+    body = await request.read()
+
+    secret = os.environ.get("GH_SECRET")
+    oauth_token = os.environ.get("GH_AUTH")
+
+    # A representation of a GitHub webhook event
+    event = sansio.Event.from_http(request.headers, body, secret=secret)
+
+    async with aiohttp.ClientSession() as session:
+        gh = gh_aiohttp.GitHubAPI(session, "williln", oauth_token=oauth_token)
+        # Call the appropriate callback for the event
+        await router.dispatch(event, gh)
+
+    # Return a "Success"
+    return web.Response(status=200)
 
 
 if __name__ == "__main__":
